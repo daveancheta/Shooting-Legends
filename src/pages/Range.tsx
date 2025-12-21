b9aaad078b7ac41b8e87a2d43473cde9aab386d7
Heaven Dave Ancheta <daveancheta8@gmail.com>
2025-12-21 23:52:50 +0800

    feat: auto move characters

--- src/pages/Range.tsx
+++ src/pages/Range.tsx
@@ -1,10 +1,63 @@
+import { useEffect, useRef } from "react"
+
 function Range() {
-  return (
-    <div>
-        <img className='fixed top-10 left-10 w-40 h-40' src="saitama.png" alt="saitama" />
-        <img className='fixed top-50 right-10 w-40 h-40' src="mario.png" alt="saitama" />
-    </div>
-  )
+    const saitama = useRef<HTMLImageElement | null>(null)
+    const mario = useRef<HTMLImageElement | null>(null)
+    const speed = 1;
+
+    const leftPos = useRef<number>(10)
+    const rightPost = useRef<number>(10)
+
+    useEffect(() => {
+
+        const autoRun = () => {
+            leftPos.current += speed;
+            rightPost.current += speed;
+
+            if (saitama.current) {
+                saitama.current.style.left = leftPos.current + "px"
+            }
+
+            if (mario.current) {
+                mario.current.style.right = rightPost.current + "px"
+            }
+
+            if (leftPos.current > window.innerWidth) {
+                leftPos.current = -110;
+            }
+
+            if (rightPost.current > window.innerWidth) {
+                rightPost.current = -110;
+            }
+
+            requestAnimationFrame(autoRun)
+        }
+
+        autoRun()
+
+    })
+    return (
+        <div>
+            <img ref={saitama}
+                style={{
+                    position: "fixed",
+                    top: "10px",
+                    left: "10px",
+                    width: "150px",
+                    height: "150px"
+                }}
+                src="saitama.png" alt="saitama" />
+            <img ref={mario}
+                style={{
+                    position: "fixed",
+                    top: "150px",
+                    right: "10px",
+                    width: "150px",
+                    height: "150px"
+                }}
+                src="mario.png" alt="saitama" />
+        </div>
+    )
 }
 
 export default Range
