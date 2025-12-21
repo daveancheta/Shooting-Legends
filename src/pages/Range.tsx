f9cf4ea06ad4d1605f03d544bb202ea4c051b954
Heaven Dave Ancheta <daveancheta8@gmail.com>
2025-12-21 23:23:32 +0800

    feat: add new page and characters

---
+++ src/pages/Range.tsx
@@ -0,0 +1,10 @@
+function Range() {
+  return (
+    <div>
+        <img className='fixed top-10 left-10 w-40 h-40' src="saitama.png" alt="saitama" />
+        <img className='fixed top-50 right-10 w-40 h-40' src="mario.png" alt="saitama" />
+    </div>
+  )
+}
+
+export default Range
